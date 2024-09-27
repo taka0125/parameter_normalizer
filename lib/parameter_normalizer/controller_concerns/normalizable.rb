99d7844b490099187f915f7776a4636d6f15349c
Takahiro Ooishi <taka0125@gmail.com>
2024-09-27 13:20:34 +0900

    :+1: add normalize_parameter

--- lib/parameter_normalizer/controller_concerns/normalizable.rb
+++ lib/parameter_normalizer/controller_concerns/normalizable.rb
@@ -6,6 +6,14 @@ module ParameterNormalizer
       def parameter_normalizer
         @parameter_normalizer ||= ParameterNormalizer::Normalizer.new
       end
+
+      def normalize_parameter(key, method, skip_key_not_found: true)
+        if skip_key_not_found
+          return unless params.key?(key)
+        end
+
+        params[key] = parameter_normalizer.public_send(method, params[key])
+      end
     end
   end
 end
